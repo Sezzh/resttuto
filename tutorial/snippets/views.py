b837736618808b304b145101b0540343a788058f
sezzh <sezzhltd@gmail.com>
2016-06-03 23:42:09 -0500

    test with generic views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import (
     SnippetModelSerializer as SnippetSerializer, UserSerializer
@@ -28,7 +30,33 @@ class SnippetList(generics.ListCreateAPIView):
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
+    # queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        snippet_pk = self.kwargs['snippet_pk']
+        try:
+            return Snippet.objects.get(pk=snippet_pk)
+        except DoesNotExist:
+            raise Http404
+
+    # muchas dudas aqui
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = queryset
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class UserSnippetList(generics.ListAPIView):
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Snippet.objects.filter(owner_id=pk)
+
+    def list(self, request, pk):
+        queryset = self.get_queryset()
+        serializer = SnippetSerializer(queryset, many=True)
+        return Response(serializer.data)
